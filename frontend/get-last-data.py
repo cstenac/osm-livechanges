1b1a1311ed78c34077bbb1a098cf97e82af7776a
Clément Stenac <clement.stenac@gmail.com>
2012-08-26 10:38:31 +0200

    osm-livechanges frontend v0:
     * Download aggregates per 10s of the changeset data
     * Show them on both a leaflet map and highcharts graphs

---
+++ frontend/get-last-data.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+import sys, time, datetime, math, sqlite3, json, cgi
+
+print "Content-Type: application/json"
+print ""
+
+data = {}
+ts = []
+
+max_delay = 1800
+
+now = int(time.time())
+now_aligned = now - now % 10
+beg = (now - max_delay)
+
+db = sqlite3.connect("/home/zorglub/public_html/livechanges/backend/changesets.db")
+db.row_factory = sqlite3.Row
+c = db.cursor()
+
+rows = []
+max_valid_timestamp = 0
+#print "Working from %s to %s" % (beg, now_aligned)
+for row in c.execute("SELECT * from changeset where time > ? ORDER BY time", (beg,)):
+    rows.append(row)
+    max_valid_timestamp = row["time"]
+
+data["debug"] = "I want data from %s to %s and my max is %s" % (now_aligned - max_delay, now_aligned, max_valid_timestamp)
+#print "Studying %i rows " % len(rows)
+
+def copy(row):
+   x = {}
+   for key in row.keys():
+       x[key] = row[key]
+   return x
+
+# TODO: Really gruik
+for time in xrange(now_aligned - max_delay, now_aligned, 10):
+    e = {"time" : time, "nbChangesets" : 0, "totalActivity": 0,
+    "cnode" : 0, "mnode" : 0, "dnode" : 0,
+    "cway" : 0, "mway" : 0, "dway" : 0,
+    "crel" : 0, "mrel" : 0, "drel" : 0, "changesets" : []}
+#    print "look for data for %s" % time
+    for row in rows:
+        rowTime = int(row["time"])
+#        print "  changeset is at %s" % rowTime
+        if rowTime >= time and rowTime < time + 10:
+ #           print "Found candidate changeset"
+            # Keep this changeset for this time entry
+            e["changesets"].append(copy(row))
+#{ "id" : row["id"], "user": row["user"],
+#                        "minLon" : row["minLon"
+            e["cnode"] += int(row["cnode"])
+            e["mnode"] += int(row["mnode"])
+            e["dnode"] += int(row["dnode"])
+            e["cway"] += int(row["cway"])
+            e["mway"] += int(row["mway"])
+            e["dway"] += int(row["dway"])
+            e["crel"] += int(row["crel"])
+            e["mrel"] += int(row["mrel"])
+            e["drel"] += int(row["drel"])
+            e["nbChangesets"] += 1
+    e["totalActivity"] += e["cnode"] + e["mnode"] + e["dnode"]
+    e["totalActivity"] += e["cway"] + e["mway"] + e["dway"]
+    e["totalActivity"] += e["crel"] + e["mrel"] + e["drel"]
+    ts.append(e)
+    if time > max_valid_timestamp:
+        break
+
+data["timeseries"] = ts
+print json.dumps(data)
+
+
