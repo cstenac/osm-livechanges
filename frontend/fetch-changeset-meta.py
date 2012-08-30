de7d6e595691604d4edf6dbf733db07078902ae9
Clément Stenac <clement.stenac@gmail.com>
2012-08-30 09:41:27 +0200

    Add frontend script to lazily fetch changeset meta data + store it persistently (so that multiple users will only request it once)

---
+++ frontend/fetch-changeset-meta.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+import sys, time, datetime, math, sqlite3, json, cgi, os, urllib2
+from lxml import etree
+
+print "Content-Type: application/json"
+print ""
+
+
+changesetId = int(os.environ["QUERY_STRING"].split('=')[1])
+
+data = { "id": changesetId, "tags" : {}, "requested" : False }
+
+db = sqlite3.connect("/home/zorglub/public_html/livechanges/backend/changesets.db")
+db.row_factory = sqlite3.Row
+c = db.cursor()
+
+found = False
+for row in c.execute("SELECT * from changeset_meta where changeset_id=?", (changesetId,)):
+    found = True
+if found:
+    for row in c.execute("SELECT * from changeset_meta_tag where changeset_id=?", (changesetId,)):
+        data["tags"][row["key"]] = row["value"];    
+    print json.dumps(data)
+    sys.exit(0)
+
+# Request the API, and save in DB
+xml = etree.parse("http://openstreetmap.org/api/0.6/changeset/%s?contact=clement.stenac@gmail.com&client=live.openstreetmap.fr" % changesetId)
+data["requested"] = True
+    
+c.execute("INSERT INTO changeset_meta (changeset_id) VALUES(?)",(changesetId,));
+
+for tag in xml.xpath("changeset/tag"):
+    data["tags"][tag.get("k")] = tag.get("v")
+    c.execute("INSERT INTO changeset_meta_tag (changeset_id,key,value) VALUES(?,?,?)",(changesetId,tag.get("k"), tag.get("v")));
+
+db.commit()
+print json.dumps(data)
+
+
