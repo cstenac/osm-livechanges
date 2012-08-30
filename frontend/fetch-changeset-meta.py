b29d5ef2f812dff0a267cbd2631daaec6d238d1d
Clément Stenac <clement.stenac@gmail.com>
2012-08-30 20:46:08 +0200

    Update / Fix ability to get changeset meta
    Fixes #6

--- frontend/fetch-changeset-meta.py
+++ frontend/fetch-changeset-meta.py
@@ -20,18 +20,16 @@ for row in c.execute("SELECT * from changeset_meta where changeset_id=?", (chang
 if found:
     for row in c.execute("SELECT * from changeset_meta_tag where changeset_id=?", (changesetId,)):
         data["tags"][row["key"]] = row["value"];    
-    print json.dumps(data)
-    sys.exit(0)
+else:
+    # Request the API, and save in DB
+    xml = etree.parse("http://openstreetmap.org/api/0.6/changeset/%s?contact=clement.stenac@gmail.com&client=live.openstreetmap.fr" % changesetId)
+    data["requested"] = True
 
-# Request the API, and save in DB
-xml = etree.parse("http://openstreetmap.org/api/0.6/changeset/%s?contact=clement.stenac@gmail.com&client=live.openstreetmap.fr" % changesetId)
-data["requested"] = True
-    
-c.execute("INSERT INTO changeset_meta (changeset_id) VALUES(?)",(changesetId,));
+    c.execute("INSERT INTO changeset_meta (changeset_id) VALUES(?)",(changesetId,));
 
-for tag in xml.xpath("changeset/tag"):
-    data["tags"][tag.get("k")] = tag.get("v")
-    c.execute("INSERT INTO changeset_meta_tag (changeset_id,key,value) VALUES(?,?,?)",(changesetId,tag.get("k"), tag.get("v")));
+    for tag in xml.xpath("changeset/tag"):
+        data["tags"][tag.get("k")] = tag.get("v")
+        c.execute("INSERT INTO changeset_meta_tag (changeset_id,key,value) VALUES(?,?,?)",(changesetId,tag.get("k"), tag.get("v")));
 
 db.commit()
 print json.dumps(data)
